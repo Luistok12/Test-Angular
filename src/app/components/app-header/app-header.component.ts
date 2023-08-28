0e8aff7d33b1df3c131e56b0c58a3844f9ac5a9b
Luis Amadeo Tolaba Cari <cabj_luis93@hotmail.com>
2023-08-28 15:03:21 -0300

    Agrega archivo .gitignore y excluye node_modules, Header creado pero aun no responsivo

---
+++ src/app/components/app-header/app-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector modificado de app-app-header a app-header, se lo puede modificar sin problemas
+  selector: 'app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.less']
+})
+export class AppHeaderComponent {
+
+}
