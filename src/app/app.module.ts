c51ec273aef6b3faff462da6943f878d4b99615b
Luis Amadeo Tolaba Cari <cabj_luis93@hotmail.com>
2023-08-30 18:33:36 -0300

    sidebar agregado pero no completo y scrollbar personalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppFooterComponent } from './components/app-footer/app-footer.component';
 import { ContentComponent } from './components/content/content.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     AppFooterComponent,
-    ContentComponent
+    ContentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
